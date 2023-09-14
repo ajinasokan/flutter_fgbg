24c7e63a32935eee4b0a645b8e232716d8ab70bb
Stefan Schaller <stefan.schaller@tapped.dev>
2023-09-14 11:46:04 +0100

    remove not correct test code

--- android/src/main/kotlin/com/ajinasokan/flutter_fgbg/FlutterFGBGPlugin.kt
+++ android/src/main/kotlin/com/ajinasokan/flutter_fgbg/FlutterFGBGPlugin.kt
@@ -15,61 +15,61 @@ import io.flutter.plugin.common.MethodCall
 import io.flutter.plugin.common.MethodChannel
 
 /** FlutterFgbgPlugin */
-class FlutterFGBGPlugin: FlutterPlugin, ActivityAware, LifecycleObserver,
-  EventChannel.StreamHandler, MethodChannel.MethodCallHandler {
+class FlutterFGBGPlugin : FlutterPlugin, ActivityAware, LifecycleObserver,
+    EventChannel.StreamHandler, MethodChannel.MethodCallHandler {
 
-  private var lifecycleSink: EventSink? = null
-  private var _currentValue: String? = null
+    private var lifecycleSink: EventSink? = null
+    private var _currentValue: String? = null
 
-  override fun onListen(arguments: Any?, events: EventSink?) {
-    lifecycleSink = events
-  }
-
-  override fun onCancel(o: Any) {
-    lifecycleSink = null
-  }
-
-  override fun onAttachedToEngine(flutterPluginBinding: FlutterPluginBinding) {
-    EventChannel(flutterPluginBinding.binaryMessenger, "com.ajinasokan.flutter_fgbg/events")
-      .setStreamHandler(this)
-
-    MethodChannel(flutterPluginBinding.binaryMessenger, "com.ajinasokan.flutter_fgbg/method")
-      .setMethodCallHandler(this)
-  }
-
-  override fun onDetachedFromEngine(binding: FlutterPluginBinding) {}
-
-  override fun onAttachedToActivity(binding: ActivityPluginBinding) {
-    ProcessLifecycleOwner.get().lifecycle.addObserver(this)
-  }
-
-  @OnLifecycleEvent(Lifecycle.Event.ON_STOP)
-  fun onAppBackgrounded() {
-    val value = "background"
-    _currentValue = value
-    lifecycleSink?.success(value)
-  }
-
-  @OnLifecycleEvent(Lifecycle.Event.ON_START)
-  fun onAppForegrounded() {
-    val value = "foreground"
-    _currentValue = value
-    lifecycleSink?.success(value)
-  }
-
-  override fun onDetachedFromActivityForConfigChanges() {}
-
-  override fun onReattachedToActivityForConfigChanges(binding: ActivityPluginBinding) {}
-
-  override fun onDetachedFromActivity() {
-    ProcessLifecycleOwner.get().lifecycle.removeObserver(this)
-  }
-
-  override fun onMethodCall(call: MethodCall, result: MethodChannel.Result) {
-    if ((call.method == "current")) {
-      result.success(_currentValue)
-    } else {
-      result.notImplemented()
+    override fun onListen(arguments: Any?, events: EventSink?) {
+        lifecycleSink = events
     }
-  }
+
+    override fun onCancel(arguments: Any?) {
+        lifecycleSink = null
+    }
+
+    override fun onAttachedToEngine(flutterPluginBinding: FlutterPluginBinding) {
+        EventChannel(flutterPluginBinding.binaryMessenger, "com.ajinasokan.flutter_fgbg/events")
+            .setStreamHandler(this)
+
+        MethodChannel(flutterPluginBinding.binaryMessenger, "com.ajinasokan.flutter_fgbg/method")
+            .setMethodCallHandler(this)
+    }
+
+    override fun onDetachedFromEngine(binding: FlutterPluginBinding) {}
+
+    override fun onAttachedToActivity(binding: ActivityPluginBinding) {
+        ProcessLifecycleOwner.get().lifecycle.addObserver(this)
+    }
+
+    @OnLifecycleEvent(Lifecycle.Event.ON_STOP)
+    fun onAppBackgrounded() {
+        val value = "background"
+        _currentValue = value
+        lifecycleSink?.success(value)
+    }
+
+    @OnLifecycleEvent(Lifecycle.Event.ON_START)
+    fun onAppForegrounded() {
+        val value = "foreground"
+        _currentValue = value
+        lifecycleSink?.success(value)
+    }
+
+    override fun onDetachedFromActivityForConfigChanges() {}
+
+    override fun onReattachedToActivityForConfigChanges(binding: ActivityPluginBinding) {}
+
+    override fun onDetachedFromActivity() {
+        ProcessLifecycleOwner.get().lifecycle.removeObserver(this)
+    }
+
+    override fun onMethodCall(call: MethodCall, result: MethodChannel.Result) {
+        if ((call.method == "current")) {
+            result.success(_currentValue)
+        } else {
+            result.notImplemented()
+        }
+    }
 }
