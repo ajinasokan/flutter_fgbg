9e7f054c051aa2a5ebcc4cb1c68922a1bfd78ab8
Chris Sells <csells@sellsbrothers.com>
2024-07-20 19:32:48 -0700

    added web and macOS support

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,3 +1,6 @@
+import 'dart:io';
+
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_fgbg/flutter_fgbg.dart';
 import 'package:image_picker/image_picker.dart';
@@ -16,8 +19,9 @@ class MyApp extends StatefulWidget {
 
 class _MyAppState extends State<MyApp> with WidgetsBindingObserver {
   final picker = ImagePicker();
-
   List<String> events = [];
+  bool get _webOrMobile => kIsWeb || Platform.isAndroid || Platform.isIOS;
+  bool get _mobile => !kIsWeb && (Platform.isAndroid || Platform.isIOS);
 
   @override
   void didChangeAppLifecycleState(AppLifecycleState state) {
@@ -46,6 +50,7 @@ class _MyAppState extends State<MyApp> with WidgetsBindingObserver {
               child: Column(
                 crossAxisAlignment: CrossAxisAlignment.start,
                 children: <Widget>[
+                  const Text('All platforms:'),
                   ElevatedButton(
                     onPressed: () {
                       events.clear();
@@ -53,62 +58,79 @@ class _MyAppState extends State<MyApp> with WidgetsBindingObserver {
                     },
                     child: const Text("Clear logs"),
                   ),
+                  const SizedBox(height: 16),
+                  const Text('Web + Mobile only:'),
                   ElevatedButton(
-                    onPressed: () async {
-                      events.add("// Opening camera");
-                      setState(() {});
-                      await picker.pickImage(source: ImageSource.camera);
-                    },
+                    onPressed: !_webOrMobile
+                        ? null
+                        : () async {
+                            events.add("// Opening camera");
+                            setState(() {});
+                            await picker.pickImage(source: ImageSource.camera);
+                          },
                     child: const Text("Take Image"),
                   ),
                   ElevatedButton(
-                    onPressed: () async {
-                      events.add("// Opening gallery");
-                      setState(() {});
-                      await picker.pickImage(source: ImageSource.gallery);
-                    },
+                    onPressed: !_webOrMobile
+                        ? null
+                        : () async {
+                            events.add("// Opening gallery");
+                            setState(() {});
+                            await picker.pickImage(source: ImageSource.gallery);
+                          },
                     child: const Text("Pick Image"),
                   ),
                   ElevatedButton(
-                    onPressed: () async {
-                      events.add(
-                          "// Opening camera but ignoring events during this");
-                      setState(() {});
-                      FGBGEvents.ignoreWhile(() async {
-                        await picker.pickImage(source: ImageSource.camera);
-                      });
-                    },
+                    onPressed: !_webOrMobile
+                        ? null
+                        : () async {
+                            events.add(
+                                "// Opening camera but ignoring events during this");
+                            setState(() {});
+                            FGBGEvents.ignoreWhile(() async {
+                              await picker.pickImage(
+                                  source: ImageSource.camera);
+                            });
+                          },
                     child: const Text("Take Image ignoreWhile"),
                   ),
                   ElevatedButton(
-                    onPressed: () async {
-                      events.add(
-                          "// Opening gallery but ignoring events during this");
-                      setState(() {});
+                    onPressed: !_webOrMobile
+                        ? null
+                        : () async {
+                            events.add(
+                                "// Opening gallery but ignoring events during this");
+                            setState(() {});
 
-                      FGBGEvents.ignoreWhile(() async {
-                        await picker.pickImage(source: ImageSource.gallery);
-                      });
-                    },
+                            FGBGEvents.ignoreWhile(() async {
+                              await picker.pickImage(
+                                  source: ImageSource.gallery);
+                            });
+                          },
                     child: const Text("Pick Image ignoreWhile"),
                   ),
+                  const SizedBox(height: 16),
+                  const Text('Mobile only:'),
                   ElevatedButton(
-                    onPressed: () async {
-                      events.add("// Prompting biometric");
-                      setState(() {});
-                      var auth = LocalAuthentication();
+                    onPressed: !_mobile
+                        ? null
+                        : () async {
+                            events.add("// Prompting biometric");
+                            setState(() {});
+                            var auth = LocalAuthentication();
 
-                      await auth.authenticate(
-                        // biometricOnly: true,
-                        options: const AuthenticationOptions(
-                          biometricOnly: true,
-                        ),
-                        localizedReason: 'Test',
-                      );
-                    },
+                            await auth.authenticate(
+                              // biometricOnly: true,
+                              options: const AuthenticationOptions(
+                                biometricOnly: true,
+                              ),
+                              localizedReason: 'Test',
+                            );
+                          },
                     child: const Text("FaceID"),
                   ),
                   const SizedBox(height: 16),
+                  const Text('Events:'),
                   Expanded(
                     child: ListView(
                       children: [for (var e in events) Text(e)],
