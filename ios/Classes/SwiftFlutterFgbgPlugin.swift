d258d2da7a520e3786bef160a6cc0fd50b4079c0
Philippe Dupuis <philippe@braver.health>
2026-03-03 20:27:26 -0500

    debug: add logging to investigate didBecomeActive timing

--- ios/Classes/SwiftFlutterFgbgPlugin.swift
+++ ios/Classes/SwiftFlutterFgbgPlugin.swift
@@ -7,6 +7,7 @@ public class SwiftFlutterFGBGPlugin: NSObject, FlutterPlugin, FlutterStreamHandl
     private var isFirstActivation = true
 
     public static func register(with registrar: FlutterPluginRegistrar) {
+        print("[BackgroundDetector] FGBG plugin register() called")
         let instance = SwiftFlutterFGBGPlugin()
 
         let lifeCycleChannel = "com.ajinasokan.flutter_fgbg/events"
@@ -28,32 +29,42 @@ public class SwiftFlutterFGBGPlugin: NSObject, FlutterPlugin, FlutterStreamHandl
                                        selector: #selector(didBecomeActive),
                                        name: UIApplication.didBecomeActiveNotification,
                                        object: nil)
+
+        print("[BackgroundDetector] FGBG plugin observers registered, isFirstActivation=\(instance.isFirstActivation)")
     }
 
     public func onListen(withArguments arguments: Any?,
                          eventSink: @escaping FlutterEventSink) -> FlutterError? {
+        print("[BackgroundDetector] FGBG onListen called, eventSink is now set, isFirstActivation=\(isFirstActivation)")
         self.eventSink = eventSink
         return nil
     }
 
     public func onCancel(withArguments arguments: Any?) -> FlutterError? {
+        print("[BackgroundDetector] FGBG onCancel called")
         eventSink = nil
         return nil
     }
 
     @objc func didEnterBackground() {
+        print("[BackgroundDetector] FGBG didEnterBackground called, eventSink=\(eventSink != nil ? "set" : "nil")")
         self.eventSink?("background")
     }
 
     @objc func willEnterForeground() {
+        print("[BackgroundDetector] FGBG willEnterForeground called, eventSink=\(eventSink != nil ? "set" : "nil"), isFirstActivation=\(isFirstActivation)")
         isFirstActivation = false
         self.eventSink?("foreground")
     }
 
     @objc func didBecomeActive() {
+        print("[BackgroundDetector] FGBG didBecomeActive called, eventSink=\(eventSink != nil ? "set" : "nil"), isFirstActivation=\(isFirstActivation)")
         if isFirstActivation {
             isFirstActivation = false
+            print("[BackgroundDetector] FGBG didBecomeActive: emitting foreground (first activation)")
             self.eventSink?("foreground")
+        } else {
+            print("[BackgroundDetector] FGBG didBecomeActive: skipping (not first activation)")
         }
     }
 }
